c88125eb9b23ae4f1ef0376877ddcf423b99e61f
Shyam Agarwal <shyam@sumedhasoftech.com>
2021-07-31 14:07:22 +0530

    Add gitignore

---
+++ MyFightBook/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyFightBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
